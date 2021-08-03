237eaec08ae26fe18a05b0cc98367f539bba079f
Ken Nyame <hmahaddev93@gmail.com>
2021-08-03 04:34:00 -0400

    project created.
    project created.

---
+++ MusicTracks/MusicTracks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicTracks
+//
+//  Created by Khatib Mahad H. on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
